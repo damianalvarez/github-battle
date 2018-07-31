62a1cce2ece23d4224b35751a630e06661369ef6
Damián Alvarez <alvarezdami93@gmail.com>
2018-07-31 16:47:00 -0300

    Add react-router

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,28 @@
 import React from 'react'
+import { 
+    BrowserRouter as Router, 
+    Route,
+    Switch
+} from 'react-router-dom'
+import Battle from './Battle'
+import Home from './Home'
+import Nav from './Nav'
 import Popular from './Popular'
+import Results from './Results'
 
-export default class App extends React.Component {
-    
-    render() {
-        return (
-            <div className='container'>
-                <Popular />
-            </div>
-        )
-    }
-}
+const App = () => (
+    <Router>
+        <div className='container'>
+            <Nav />
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/battle' component={Battle} />
+                <Route path='/popular' component={Popular} />
+                <Route path='/battle/results' component={Results} />
+                <Route render={() => <p>Not found!</p>} />
+            </Switch>
+        </div>
+    </Router>
+)
+
+export default App
